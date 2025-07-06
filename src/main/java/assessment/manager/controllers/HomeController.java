0e77a4804e48e537b409cffabf68ba80e68a2e76
iam-fabulous <emmanuelebukainnah@gmail.com>
2025-07-06 23:03:37 +0100

    ReFix MongoDB database name mismatch

--- src/main/java/assessment/manager/controllers/HomeController.java
+++ src/main/java/assessment/manager/controllers/HomeController.java
@@ -1,4 +1,12 @@
 package assessment.manager.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Assessment Management System!";
+    }
 }
